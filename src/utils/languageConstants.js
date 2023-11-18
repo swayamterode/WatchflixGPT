9dd01efab756c4c7416eff7c83f8d78a29a3fdc4
swayamterode <swayam.programmer@gmail.com>
2023-11-19 01:46:24 +0530

    Multi Language on HomePage

--- src/utils/languageConstants.js
+++ src/utils/languageConstants.js
@@ -2,17 +2,30 @@ const lang = {
     en: {
         search: "Search",
         gptSearchHeading: "Search for your favorite movies, TV shows and more.",
-        gptSearchPlaceholder: "Happy? Sad? Angry? Excited? What are you in the mood for?",
+        gptSearchPlaceholder: "What are you in the mood for?",
+        nowPlayingMovies: "Now Playing Movies",
+        popularMovies: "Popular Movies",
+        topRatedMovies:"Top Rated Movies",
+        upcomingMovies: "Upcoming Movies",
+
     },
     hindi: {
         search: "खोज",
         gptSearchHeading: "अपनी पसंदीदा फिल्में, टीवी शो और बहुत कुछ खोजें।",
         gptSearchPlaceholder: "आपका मिजाज कैसा है",
+        nowPlayingMovies: "अभी फ़िल्में चल रही हैं",
+        popularMovies: "लोकप्रिय फ़िल्में",
+        topRatedMovies:"शीर्ष रेटेड फिल्में",
+        upcomingMovies: "आगामी फिल्में",
     },
     spanish: {
         search: "Buscar",
         gptSearchHeading: "Busque sus películas, programas de TV y más favoritos.",
         gptSearchPlaceholder: "¿Feliz? ¿Triste? ¿Enojado? ¿Entusiasmado? ¿Para qué estás de humor?",
+        nowPlayingMovies: "Ahora reproduciendo películas",
+        popularMovies: "Películas populares",
+        topRatedMovies:"Películas mejor calificadas",
+        upcomingMovies: "Próximas películas",
     },
 };
 export default lang;
