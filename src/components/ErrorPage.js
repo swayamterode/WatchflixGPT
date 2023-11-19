86d96113319546d328279a625f5c8fb06d7e445a
swayamterode <swayam.programmer@gmail.com>
2023-11-19 20:30:51 +0530

    404 Error Page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+const Error = () => {
+    return (
+        <>
+            <Header />
+            <section class="bg-gray-900">
+                <div class="py-8 px-4 mx-auto max-w-screen-xl h-screen lg:py-16 lg:px-6">
+                    <div class="mt-[20%]  mx-auto max-w-screen-sm text-center">
+                        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-500">404</h1>
+                        <p class="mb-4 text-3xl tracking-tight font-bold  md:text-4xl text-white">Something's missing.</p>
+                        <p class="mb-4 text-lg font-light text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                        <Link to="/" class="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-blue-900 my-4">Back to Homepage</Link>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Error;
