ea1befcf95294da9458e4e1f3b693ea59c1138c9
swayamterode <swayam.programmer@gmail.com>
2023-12-07 01:29:15 +0530

    Watchflix Deployed 🎉

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,12 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXRfQ6LDnh04MyYiSu0BUhwRAI6RIZ9UM",
-  authDomain: "watchflix-ai.firebaseapp.com",
-  projectId: "watchflix-ai",
-  storageBucket: "watchflix-ai.appspot.com",
-  messagingSenderId: "72712492500",
-  appId: "1:72712492500:web:25e9e9446b28b4dea07241"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
